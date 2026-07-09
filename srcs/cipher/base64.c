90b9ff6fb6cfb6ac46fd28d8a37a81f9992952fc
klaus <antoine.krings@gmail.com>
2026-07-09 18:07:06 +0200

    fjkdsl

--- srcs/cipher/base64.c
+++ srcs/cipher/base64.c
@@ -1,5 +1,33 @@
 #include "cipher.h"
 
+static uint8_t	base64_encode6b(uint8_t	n)
+{
+	if (n > 63)
+		return ('%');
+	else if (n < 26)
+		return ('A' + n);
+	else if (n < 52)
+		return ('a' + n);
+	else if (n < 62)
+		return ('0' + n);
+	else if (n == 62)
+		return ('+');
+	else
+		return ('/');
+}
+
+void	base64_encode_chunk(uint8_t dest[4], uint8_t data[3])
+{
+	uint32_t	n;
+
+	n = data[0] + data[1] * (1 << 8) + data[2] * (1 << 16);
+	for (int i = 0; i < 4; i++)
+	{
+		dest[i] = base64_encode6b(n & 0x3f);
+		n >>= 6;
+	}
+}
+
 /*
 ** TODO: base64 encode/decode (V.0.1).
 **   flags: -e (default) / -d / -i infile / -o outfile
@@ -12,6 +40,10 @@ int	base64_command(const t_cipher *self, int argc, char **argv)
 	(void)self;
 	(void)argc;
 	(void)argv;
+	uint8_t dest[4];
+	base64_encode_chunk(dest, (uint8_t *)argv[1]);
+	write(1, dest, 4);
+	write(1, "\n", 1);
 	ft_putstr_fd("ft_ssl: base64: not yet implemented\n", 2);
 	return (1);
 }
